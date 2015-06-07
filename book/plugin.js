8a6bc3322101b367a55c98a2f5f80a0e6c7d90d2
IgitDanny <IgitDanny@mlm-spiele.de>
2015-06-07 11:59:52 +0200

    Update plugin.js

--- book/plugin.js
+++ book/plugin.js
@@ -2,7 +2,7 @@ require(["gitbook"], function(gitbook) {
   gitbook.events.bind("page.change", function() {
     $('code.lang-sequence').each(function(index, element) {
       var $element = $(element);
-      $element.sequenceDiagram({theme: 'simple'});
+      $element.sequenceDiagram({theme: 'hand'});
 
       var wrapper = $("<div class='scroll'></div>");
       wrapper.html($element.html());
